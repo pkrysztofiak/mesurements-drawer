81d148920a490de4cae22496f09893e805436a36
p.krysztofiak <p.krysztofiak@pixel.com.pl>
2019-04-27 13:07:05 +0200

    Progress

--- src/main/java/pl/pkrysztofiak/mesurementsdrawer/view/measurements/graphics/PolygonPointCircle.java
+++ src/main/java/pl/pkrysztofiak/mesurementsdrawer/view/measurements/graphics/PolygonPointCircle.java
@@ -1,20 +1,21 @@
 package pl.pkrysztofiak.mesurementsdrawer.view.measurements.graphics;
 
 import javafx.scene.shape.Circle;
-import pl.pkrysztofiak.mesurementsdrawer.view.measurements.graphics.behaviour.OnMouseHover;
 import pl.pkrysztofiak.mesurementsdrawer.view.measurements.graphics.behaviour.OnMouseHoverCircleLazy;
 
-public class PolygonPointCircle extends PointNode<Circle> implements OnMouseHover {
+public class PolygonPointCircle extends PointNode<Circle> {
 
 	public PolygonPointCircle(Circle circle) {
 		super(circle);
+		//tutaj dorzucić chyba interfejs który ma się wywoływać
 		setOnMouseHover(new OnMouseHoverCircleLazy(circle));
 //		onMouseHoverProperty.set(new OnMouseHoverCircleLazy(circle));
 	}
 
-	@Override
-	public void onMouseHover(boolean value) {
-		onMouseHover.onMouseHover(value);
-//		onMouseHoverProperty.get().onMouseHover(value);
-	}
+//	@Override
+//	public void onMouseHover(boolean value) {
+//		System.out.println("kurwa");
+//		onMouseHover.onMouseHover(value);
+////		onMouseHoverProperty.get().onMouseHover(value);
+//	}
 }
