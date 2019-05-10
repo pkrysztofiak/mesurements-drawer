af2c7712be042941002279848f854497341acedd
p.krysztofiak <p.krysztofiak@pixel.com.pl>
2019-05-10 19:56:43 +0200

    Progress

--- src/main/java/pl/pkrysztofiak/mesurementsdrawer/controller/tool/ToolController.java
+++ src/main/java/pl/pkrysztofiak/mesurementsdrawer/controller/tool/ToolController.java
@@ -4,51 +4,29 @@ import io.reactivex.Observable;
 import io.reactivex.rxjavafx.observables.JavaFxObservable;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import pl.pkrysztofiak.mesurementsdrawer.controller.panel.PanelController;
 import pl.pkrysztofiak.mesurementsdrawer.model.Model;
 import pl.pkrysztofiak.mesurementsdrawer.view.measurements.Measurement;
 
 public class ToolController {
-
-    private final Behaviour behaviour = new Behaviour();
 
     private final Model model;
 
     private final ObjectProperty<Tool> selectedToolProperty = new SimpleObjectProperty<>();
     private final Observable<Tool> selectedToolObservable = JavaFxObservable.valuesOf(selectedToolProperty);
 
-    private final ObjectProperty<PanelController> selectedPanelControllerProperty = new SimpleObjectProperty<>();
-    private final Observable<PanelController> selectedPanelControllerObservable = JavaFxObservable.valuesOf(selectedPanelControllerProperty);
-
     public ToolController(Model model) {
         this.model = model;
-        initSubscriptions();
     }
 
     public void setSelectedTool(Tool tool) {
     	selectedToolProperty.set(tool);
     }
 
-    private void initSubscriptions() {
-        selectedPanelControllerObservable.subscribe(behaviour::onSelectedPanelControllerChanged);
-    }
-
     public Observable<Measurement> measurementCreatedObservable() {
     	return selectedToolObservable.switchMap(Tool::measurementCreatedObservable);
     }
 
-    public void setSelectedPanelController(PanelController panelController) {
-        selectedPanelControllerProperty.set(panelController);
-    }
-
     public final Observable<Tool> selectedToolObservable() {
         return selectedToolObservable;
-    }
-
-    private class Behaviour {
-
-        private void onSelectedPanelControllerChanged(PanelController selectedPanelController) {
-            selectedPanelController.setEventsReceiver(selectedToolProperty.get());
-        }
     }
 }
