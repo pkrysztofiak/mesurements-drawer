42cae65a7950546344e1ffc43ec065db29432a36
p.krysztofiak <p.krysztofiak@pixel.com.pl>
2019-05-15 07:49:44 +0200

    Progress

--- src/main/java/pl/pkrysztofiak/mesurementsdrawer/view/measurements/graphics/point/CirclePointView.java
+++ src/main/java/pl/pkrysztofiak/mesurementsdrawer/view/measurements/graphics/point/CirclePointView.java
@@ -1,17 +1,40 @@
 package pl.pkrysztofiak.mesurementsdrawer.view.measurements.graphics.point;
 
+import io.reactivex.Observable;
+import io.reactivex.rxjavafx.observables.JavaFxObservable;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import pl.pkrysztofiak.mesurementsdrawer.model.measurements.Point;
 
 public class CirclePointView extends PointView {
 
-	private final Circle circle = new Circle(8., Color.CYAN);
+	private final Behaviour behaviour = new Behaviour();
+
+	private final Paint hoverPaint = Color.CYAN;
+	private final Paint paint = Color.BISQUE;
+
+	private final Circle circle = new Circle(8., paint);
+	private final Observable<Boolean> hoverObservable = JavaFxObservable.valuesOf(circle.hoverProperty());
+	{
+		getChildren().add(circle);
+	}
 
 	public CirclePointView(Point point) {
 		super(point);
-		getChildren().add(circle);
 		circle.layoutXProperty().bindBidirectional(point.layoutXProperty());
 		circle.layoutYProperty().bindBidirectional(point.layoutYProperty());
+		initSubscriptions();
+	}
+
+	private void initSubscriptions() {
+		hoverObservable.subscribe(behaviour::onHover);
+	}
+
+	private class Behaviour {
+
+		private void onHover(boolean value) {
+			circle.setFill(value ? hoverPaint : paint);
+		}
 	}
 }
